1fe36fe60d2586689f532e314c90684c3c04daf8
Josh <jr8ers@gmail.com>
2020-07-21 18:44:42 -0500

    new

---
+++ bot.py
@@ -0,0 +1,31 @@
+import asyncio
+import requests
+import discord
+import json
+
+
+from discord.ext import commands, tasks
+
+#bot = commands.Bot(command_prefix='t!')
+
+bot = discord.Client()
+
+activity=True
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(status=discord.Status.online,
+                                 activity=discord.Activity(name="your bank accounts drain.", type=3))
+    print(f'{bot.user.name} is running...')
+
+
+@bot.event
+async def on_message(message):
+    if message.author != bot.user:
+        if 'vandy' in message.content:
+            await message.channel.send('fuck vandy')
+            await message.channel.send('all my homies hate vandy')
+        elif ('palpatine' in message.content) or ('emperor' in message.content) or ('diermeier' in message.content) or ('chancellor' in message.content):
+            await message.channel.send('https://media.discordapp.net/attachments/699763540361478145/734157709770752131/image0.jpg')
+
+bot.run('NzM1MjY2NDQxMjA0MzM0NjYz.XxdwQw.rS-OMXxR-vyPllg1OmZx_ZlNjVo')

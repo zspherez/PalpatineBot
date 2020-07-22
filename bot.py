5e255a0e1bef8c382c2fd0ebaebac83ee0400620
Josh <jr8ers@gmail.com>
2020-07-22 18:58:00 -0500

    fixed

--- bot.py
+++ bot.py
@@ -30,8 +30,12 @@ async def on_message(message):
                 await message.channel.send('Towers Floor Plans: https://discordapp.com/channels/678041940901625865/678067983376973845/735613872098246768')
             elif 'branscomb' in message.content.lower():
                 await message.channel.send('Branscomb Floor Plans: https://discordapp.com/channels/678041940901625865/678067983376973845/725169006113325197')
-        if 'https://groupme' in message.content.lower()
+        if 'https://groupme' in message.content.lower():
             await message.author.send('We do not allow advertising of GroupMe\'s in the Discord, your message has been deleted. If this is a house/floor GroupMe, please wait until the house channels have been created.')
             await message.delete()
+        if 'vandy' in message.content.lower() and random.randint(0,19) == 7:
+            await message.channel.send('fuck vandy')
+            await message.channel.send('all my homies hate vandy')
+
 
 bot.run(os.environ['TOKEN'])

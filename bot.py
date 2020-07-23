da6c78ba64f2d6702e1ad1290b443edc40dee138
Josh <jr8ers@gmail.com>
2020-07-23 00:08:20 -0500

    added semicolon

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ async def on_message(message):
             await message.delete()
         if 'vandy' in message.content.lower():
             rand = random.randint(0, 14)
-            if rand == 7;
+            if rand == 7:
                 await message.channel.send('fuck vandy')
                 await message.channel.send('all my homies hate vandy')
         if 'emperor' in message.content.lower() or 'palpatine' in message.content.lower() or 'diermeier' in message.content.lower() or 'chancellor' in message.content.lower():
@@ -44,6 +44,5 @@ async def on_message(message):
             if rand == 7:
                 await message.channel.send('https://media.discordapp.net/attachments/699763540361478145/734157709770752131/image0.jpg')
                 await message.channel.send('The Emperor is always watching...')
-
 
 bot.run(os.environ['TOKEN'])

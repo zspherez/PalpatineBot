838fa52a92bf4b568ef5593af35d0c9503548e32
Josh <jr8ers@gmail.com>
2020-08-12 01:23:17 -0500

    Suggestions channel support?

--- bot.py
+++ bot.py
@@ -148,6 +148,9 @@ async def on_message(message):
                 await message.channel.send('The Emperor is always watching...')
         if bot.user in message.mentions:
             await message.channel.send('no :)')
+        if message.channel == 692841868777750638:
+            message.add_reaction('\N{THUMBS UP SIGN}')
+            message.add_reaction('\N{THUMBS DOWN SIGN}')
     await bot.process_commands(message)
 
 bot.run(os.environ['TOKEN'])

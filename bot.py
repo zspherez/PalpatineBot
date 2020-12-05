8a6e7520383925ac9f4581f633d61d905280e878
Josh Rehders <josh.rehders@vanderbilt.edu>
2020-12-04 23:32:05 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ from airtable import Airtable
 print(discord.__version__)
 
 intents = discord.Intents().all()
-bot = discord.Bot(prefix='.', intents=intents)
+bot = discord.commands(command_prefix='.', intents=intents)
 
 activity = True
 
